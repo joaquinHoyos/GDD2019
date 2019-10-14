897b04b76306f2139656841c3b9e90c9b757cafc
Nico <nmoncarz@gmail.com>
2019-10-14 12:44:11 -0300

    Migracion creditos

--- execProcedures.sql
+++ execProcedures.sql
@@ -1,8 +1,3 @@
-USE [GD2C2019]
-GO
-/****** Object:  Schema [PASO_A_PASO]    Script Date: 10/14/2019 12:07:23 PM ******/
-CREATE SCHEMA [PASO_A_PASO]
-GO
 EXEC PASO_A_PASO.creacionTablas;
 EXEC PASO_A_PASO.keys;
 EXEC PASO_A_PASO.migracionTablas;
