3e79b4c4201a816b3a09f790e5e0429911d842e1
leoguerberg <lguerberg@est.frba.utn.edu.ar>
2019-11-05 23:21:42 -0300

    Migracion terminada (21 SEGS)

--- migracionTablas.sql
+++ migracionTablas.sql
@@ -1,10 +1,11 @@
-USE GD2C2019
-GO
-CREATE SCHEMA PASO_A_PASO AUTHORIZATION gdCupon2019
-GO
+
 
 CREATE PROCEDURE PASO_A_PASO.migracionTablas
 as
+
+	INSERT INTO PASO_A_PASO.TipoPago VALUES ('E')
+	INSERT INTO PASO_A_PASO.TipoPago VALUES ('C')
+
 
 INSERT INTO PASO_A_PASO.Cliente SELECT distinct Cli_Dni, Cli_Nombre, Cli_Apellido, null, Cli_Mail, Cli_Telefono, Cli_Direccion, 0, null, Cli_Ciudad, Cli_Fecha_Nac FROM gd_esquema.Maestra;
 INSERT INTO PASO_A_PASO.Rubro SELECT distinct Provee_Rubro FROM gd_esquema.Maestra;
@@ -18,7 +19,9 @@ INSERT INTO PASO_A_PASO.Credito
 					Carga_Credito
 			FROM gd_esquema.Maestra JOIN PASO_A_PASO.Cliente on (Cli_Dni = clie_dni)
 			WHERE Tipo_Pago_Desc is not null and Carga_Fecha is not null and Carga_Credito is not null;
-INSERT INTO PASO_A_PASO.Factura SELECT Factura_Nro,DATETIMEFROMPARTS(Year(Factura_Fecha),MONTH(Factura_Fecha),1,0,0,0,0),Factura_Fecha,sum(Oferta_Precio),prov_id from gd_esquema.Maestra join PASO_A_PASO.Proveedor on Provee_CUIT=prov_cuit and Provee_RS=prov_razon where Factura_Fecha is not null group by Factura_Nro,Factura_Fecha,prov_id
+
+INSERT INTO PASO_A_PASO.Factura SELECT Factura_Nro,DATETIMEFROMPARTS(Year(Factura_Fecha),MONTH(Factura_Fecha),1,0,0,0,0),Factura_Fecha,sum(Oferta_Precio),prov_id from gd_esquema.Maestra join PASO_A_PASO.Proveedor on Provee_CUIT=prov_cuit and Provee_RS=prov_razon where Factura_Fecha is not null group by Factura_Nro,Factura_Fecha,prov_id;
+
 INSERT INTO PASO_A_PASO.Compra SELECT t.Oferta_Codigo,c.clie_id,count(*) as cantCupones,
 (SELECT t1.Factura_Nro from gd_esquema.Maestra t1 where t1.Factura_Nro is not null and t1.Cli_Dni=t.Cli_Dni and t1.Oferta_Codigo=t.Oferta_Codigo and t1.Provee_CUIT=t.Provee_CUIT and t.Oferta_Fecha_Compra=t1.Oferta_Fecha_Compra) as Faa,
 sum(t.Oferta_Precio) as precioCompra from gd_esquema.Maestra t
