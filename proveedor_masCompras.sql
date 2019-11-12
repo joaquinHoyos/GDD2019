7a0b139fa2e3cb984a704e2add180a6011eda81a
Joaquin <jhoyos@est.frba.utn.edu.ar>
2019-11-12 19:49:57 -0300

    Funciones estadisticas

---
+++ proveedor_masCompras.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION proveedor_masFacturas(@año int,@mesInicio int,@mesFin int)
+RETURNS TABLE
+AS
+	RETURN
+	SELECT TOP 5 
+	prov_id AS 'ID'
+	,prov_razon AS 'Razon Social'
+	,prov_cuit AS 'CUIT'
+	,rubr_nombre AS 'Rubro'
+	,SUM(comp_cantidad) AS 'Cantidad de Compras'
+	,@año AS 'año'
+	,CASE WHEN @mesInicio<6 THEN 1 ELSE 2 END AS 'Semestre'
+	FROM PASO_A_PASO.Proveedor
+	JOIN PASO_A_PASO.Rubro ON (rubr_id=prov_rubro)
+	JOIN PASO_A_PASO.Oferta ON (prov_id=ofer_proveedor)
+	JOIN PASO_A_PASO.Compra ON (comp_oferta=ofer_id)
+	WHERE YEAR(ofer_fechaDesde)=@año and MONTH(ofer_fechaDesde) BETWEEN @mesInicio AND @mesFin
+	GROUP BY prov_id,prov_razon,prov_nombre,prov_cuit,rubr_nombre
+	ORDER BY 'Cantidad de Compras' DESC
+		
