76e52662635cfb1ea7691d3900959184c29efaec
Nico <nmoncarz@gmail.com>
2019-11-12 22:32:32 -0300

    avances en altaProveedor

--- Aplicacion/FrbaOfertas/FrbaOfertas/Forms/AltaProveedor.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Forms/AltaProveedor.cs
@@ -22,13 +22,42 @@ namespace FrbaOfertas.Forms
 
         private void btn_AltaProveedor_Click(object sender, EventArgs e)
         {
+            if (txt_Telefono.Value != 0 && txt_CP.Value != 0)
+            {       
+                if (txt_Ciudad.Text != "" && txt_Direccion.Text != "" && txt_Mail.Text != "" && txt_Nombre.Text != "")
+                    {
 
+
+                        //RepoUsuario.instance().altaCliente(txt_Nombre.Text, txt_Apellido.Text, Convert.ToInt64(txt_DNI.Value), Convert.ToInt32(txt_CP.Value), txt_Direccion.Text, txt_Ciudad.Text, txt_Mail.Text, Convert.ToInt64(txt_Telefono.Value), dateTimePicker1.Value);
+
+
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("Datos de texto invalidos");
+
+                    }
+
+                
+                
+            }
+            else
+            {
+
+                MessageBox.Show("Campos numericos invalidos");
+
+            }
         }
 
         private void AltaProveedor_Load(object sender, EventArgs e)
         {
             txt_Telefono.Controls[0].Visible = false;
             txt_CP.Controls[0].Visible = false;
+            comboBox1.DataSource = RepoRol.instance().buscarPorTodo();
+            comboBox1.DisplayMember= "nombre";
+            comboBox1.ValueMember = "id";
+            
         }
     }
 }
