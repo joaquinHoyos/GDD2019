ffbe44552eee361e5f1fc248dafa32fc4a9f1f0d
guidoklaj <guidoklajnberg@gmail.com>
2019-11-13 23:47:06 -0300

    arreglado sp facturar

--- facturarProveedor.sql
+++ facturarProveedor.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE PASO_A_PASO.facturarProveedor (@fechaDesde smalldatetime, @fech
 as
 if (SELECT sum(c1.comp_importe) FROM PASO_A_PASO.Compra c1 WHERE c1.comp_fecha between @fechaDesde and @fechaHasta AND c1.comp_factura is null) is not null
 begin
-INSERT INTO PASO_A_PASO.Factura (fact_desde,fact_hasta,fact_importe,fact_proveedor) VALUES (@fechaDesde,@fechaHasta,(SELECT sum(c1.comp_importe) FROM PASO_A_PASO.Compra c1 WHERE c1.comp_fecha between @fechaDesde and @fechaHasta AND c1.comp_factura is null),@prov)
+INSERT INTO PASO_A_PASO.Factura (fact_id,fact_desde,fact_hasta,fact_importe,fact_proveedor) VALUES ((SELECT TOP 1 f1.fact_id FROM PASO_A_PASO.Factura f1 ORDER BY 1 desc)+1,@fechaDesde,@fechaHasta,(SELECT sum(c1.comp_importe) FROM PASO_A_PASO.Compra c1 WHERE c1.comp_fecha between @fechaDesde and @fechaHasta AND c1.comp_factura is null),@prov)
 UPDATE PASO_A_PASO.Compra SET comp_factura=(SELECT TOP 1 fact_id FROM PASO_A_PASO.Factura ORDER BY 1 desc) WHERE comp_fecha between @fechaDesde and @fechaHasta AND comp_factura is null
 end
 else
