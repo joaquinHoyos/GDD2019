4296d38cc6243d92bf483de49b2e52a7da0fd6e3
Nico <nmoncarz@gmail.com>
2019-11-28 00:19:28 -0300

    Script inicial actualizado

--- script_creacion_inicial_completo.sql
+++ script_creacion_inicial_completo.sql
@@ -1251,5 +1251,31 @@ END
 GO
 
 
+USE [GD2C2019]
+GO
+
+/****** Object:  StoredProcedure [PASO_A_PASO].[modificarRol]    Script Date: 28/11/2019 0:14:44 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [PASO_A_PASO].[modificarRol](@id int,@nombre varchar(20), @funciones tablaFuncion READONLY)
+AS
+	IF @nombre NOT IN (SELECT rol_nombre FROM Rol WHERE rol_id=@id)
+		UPDATE Rol SET rol_nombre = @nombre WHERE rol_id=@id
+	
+
+	DELETE FuncionesxRol WHERE id_rol=@id
+
+	INSERT INTO FuncionesxRol
+	SELECT @id,funciones FROM @funciones 
+
+
+	
+GO
+
+
 
 
